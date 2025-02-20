44cab90b2dc452e69863e3d17aac500e231b3b44
BajicMladen <bajicmladen33@gmail.com>
2025-02-20 20:44:47 +0100

    Add translations.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,18 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/eslint'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/eslint', '@nuxtjs/i18n'],
   eslint: {
     config: {
       standalone: false,
     },
+  },
+  i18n: {
+    lazy: true,
+    strategy: 'prefix_except_default',
+    defaultLocale: 'en',
+    locales: [
+      { code: 'en', file: 'english/en-US.json' },
+    ],
   },
 })
